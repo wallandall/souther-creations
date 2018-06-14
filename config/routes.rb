3e68919cfc03ec1ef616a14589642eae29b2eeb7
wallandall <wallandall@gmail.com>
2018-06-14 19:13:36 +0200

    Added orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :categories
+  resources :orders, only: [:index, :show, :create, :destroy]
   root 'simple_pages#index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
