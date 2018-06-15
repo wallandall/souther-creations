fb8c406da2de5989032e1072019bcfeecdc1685d
wallandall <wallandall@gmail.com>
2018-06-16 00:13:05 +0200

    added the orders model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_210250) do
+ActiveRecord::Schema.define(version: 2018_06_15_215642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_06_15_210250) do
     t.text "description"
     t.string "image_url"
     t.integer "sort"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
