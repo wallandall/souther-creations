bae6748ac27a6ac8d804f4e2a2699bc9fd5d6bc0
wallandall <wallandall@gmail.com>
2018-06-26 18:38:53 +0200

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 Category.destroy_all
 Product.destroy_all
 
-Category.create(product_category: "Sculptures", description: "Handmade wooden and stone Sculptures", image_url: "products/s1.jpg")
-Category.create(product_category: "Beeds", description: "Handmand Jewellery, ornaments and accessaries sourced from local materials", image_url: "products/b1.jpg")
-Category.create(product_category: "Paintings", description: "A selection of locally sourced paintings", image_url: "products/p1.jpg")
+sculpture_category = Category.create(product_category: "Sculptures", description: "Handmade wooden and stone Sculptures", image_url: "products/s1.jpg")
+beed_category = Category.create(product_category: "Beeds", description: "Handmand Jewellery, ornaments and accessaries sourced from local materials", image_url: "products/b1.jpg")
+product_category = Category.create(product_category: "Paintings", description: "A selection of locally sourced paintings", image_url: "products/p1.jpg")
 
-Product.create(name: "Product1", description: "This is the first system generated product", image_url: "products/s2.jpg", category_id: "1")
-Product.create(name: "Product2", description: "This is the second system generated product", image_url: "products/p2.jpg", category_id: "2")
-Product.create(name: "Product3", description: "This is the third system generated product", image_url: "products/b2.jpg", category_id: "3")
+Product.create(name: "Product1", description: "This is the first system generated product", image_url: "products/s2.jpg", category_id: sculpture_category.id)
+Product.create(name: "Product2", description: "This is the second system generated product", image_url: "products/p2.jpg", category_id: beed_category.id )
+Product.create(name: "Product3", description: "This is the third system generated product", image_url: "products/b2.jpg", category_id: product_category.id)
