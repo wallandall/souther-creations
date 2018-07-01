d1583b5432e27aaa3d7ea14e7e1d1599922827eb
wallandall <wallandall@gmail.com>
2018-07-01 11:54:56 +0200

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :orders, only: [:index, :show, :create, :destroy]
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/landing_page'
+  get 'simple_pages/index'
 end
