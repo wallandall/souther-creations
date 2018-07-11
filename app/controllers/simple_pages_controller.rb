be69e0556ec5fa9188e3e2ec29bf2abe29d72e97
wallandall <wallandall@gmail.com>
2018-07-11 22:55:35 +0200

    Added contact form and configuration

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -13,4 +13,14 @@ class SimplePagesController < ApplicationController
 
   def contact
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: "chrisw@webe-solutions.com",
+      subject: "A new contact message from #{@name}",
+      body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :orders, only: [:index, :show, :create, :destroy]
-  root 'simple_pages#landing_page'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  post 'simple_pages/thank_you'
+  root 'simple_pages#landing_page'
 end
