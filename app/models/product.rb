b3189fefc4a6a05f78c76892927c3b71bb7dc7e2
wallandall <wallandall@gmail.com>
2018-07-21 13:40:47 +0200

    Added Comment Model and updated product styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     Product.where("LOWER(name) LIKE ?", "%#{search_term.downcase}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_desc.last
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_162451) do
+ActiveRecord::Schema.define(version: 2018_07_21_110619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2018_07_13_162451) do
     t.text "description"
     t.string "image_url"
     t.integer "sort"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "body"
+    t.integer "rating"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -61,4 +72,6 @@ ActiveRecord::Schema.define(version: 2018_07_13_162451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
