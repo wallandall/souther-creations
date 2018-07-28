66797b15f2a5579ae4caa8fa3065de8ddfab5bdb
wallandall <wallandall@gmail.com>
2018-07-28 23:52:07 +0200

    Added comments validation and pagination

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: {only_integer: true}
+
   belongs_to :user
   belongs_to :product
 
