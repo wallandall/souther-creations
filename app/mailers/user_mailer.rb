f6d85422956881fc2c96bf6d9824a9a9428ee31f
wallandall <wallandall@gmail.com>
2018-08-04 15:45:56 +0200

    Added email notification on sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
          to: 'chrisw@webe-solutions.com',
          subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Southern Creations"
+    mail(to: user.email, subject: "Welcome to #{@appname}")
+  end
 end
