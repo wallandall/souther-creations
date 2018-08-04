1a10f225f76fa99bd0e3f60460d5c860b50dbdae
wallandall <wallandall@gmail.com>
2018-08-04 14:58:11 +0200

    Added functionality for users to delete their comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, User, id: user.id
+      can :manage, :all
     else
       can :manage, User, id: user.id
+      can :manage, Comment, id: user.id
     end
   end
 end
