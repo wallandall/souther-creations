06357418a251e7db9b4734c15529751d6df41df7
wallandall <wallandall@gmail.com>
2018-09-02 00:39:43 +0200

    Restricted access to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action only: [:new, :edit, :update, :destroy] do
+  before_action only: [:new, :edit, :update, :destroy, :index, :show ] do
     redirect_to products_path, notice: 'You need to be an Admin to perform this action!' unless current_user && current_user.admin
   end
-  
+
   def index
     @orders = Order.includes(:product).all
   end
