7e9beb9992d31dc85c484e3de77d396e7e63c3cd
wallandall <wallandall@gmail.com>
2018-09-01 20:07:31 +0200

    Edit Payment Controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,7 @@ class PaymentsController < ApplicationController
   # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        @amount = (@product.price * 100).to_i,
+          amount: (@product.price * 100).to_i,
           currency: "eur",
           source: token,
           description: params[:stripeEmail],
