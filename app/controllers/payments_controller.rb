ab6b18b192ddda6de0ce9c2ea578daf904feb4af
wallandall <wallandall@gmail.com>
2018-09-01 19:53:53 +0200

    Added payment controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,31 @@
+class PaymentsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+  # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        @amount = (@product.price * 100).to_i,
+          currency: "eur",
+          source: token,
+          description: params[:stripeEmail],
+          receipt_email: params[:stripeEmail]
+      )
+      if charge.paid
+        Order.create(
+          product_id: @product.id,
+          user_id: @user.id,
+          total: @product.price
+        )
+        flash[:success] = "Your payment was processed successfully"
+      end
+
+      rescue Stripe::CardError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      end
+      redirect_to product_path(@product)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
+  post 'payments/create'
 end
